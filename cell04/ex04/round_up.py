06dff636f97418605647fed295b82847280a105e
wchuntum <wrodmchuntumkarn@gmail.com>
2025-06-17 05:13:11 +0000

    update

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,6 @@
+import math
+number = float(input("Please enter a number: "))
+
+rounded_nymber = math.ceil(number)
+
+print(f"{number}rounded up is {rounded_number}.")
---
+++ cell04/ex05/up_low.py
@@ -0,0 +1,4 @@
+user_input = input("Please enter a string: ")
+
+swapped_steing = user_input.swapcasw()
+print(f"The string with swapped case is: {swapped_string}")
