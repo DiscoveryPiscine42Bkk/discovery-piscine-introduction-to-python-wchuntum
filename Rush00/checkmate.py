311a6ceccd9ca1922ee650eac9990f85d73fe7cd
wchuntum <wrodmchuntumkarn@gmail.com>
2025-06-19 08:38:42 +0000

    update

---
+++ Rush00/checkmate.py
@@ -0,0 +1,9 @@
+class ChessPieces:
+	def __init__(self, array):
+	  self.king = []        
+self.pieces = []        
+self.board_size = len(array)
+
+def checkmate(board):
+   array = board.split()
+print(array)
